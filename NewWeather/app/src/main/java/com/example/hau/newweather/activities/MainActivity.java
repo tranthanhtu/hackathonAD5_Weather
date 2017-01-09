35e5bda6757b276c1ae414946701c4eb391566a3
Tran Thanh Tu <tranthanhtuxd2211@gmail.com>
2017-01-09 13:37:30 +0700

    updateUI

--- NewWeather/app/src/main/java/com/example/hau/newweather/activities/MainActivity.java
+++ NewWeather/app/src/main/java/com/example/hau/newweather/activities/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.hau.newweather.activities;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hau.newweather.R;
+import com.example.hau.newweather.adapters.SamplePagerAdapter;
 
-import retrofit2.Retrofit;
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.vp_main);
+        circleIndicator = (CircleIndicator) findViewById(R.id.indicator);
+        viewPager.setAdapter(new SamplePagerAdapter());
+        circleIndicator.setViewPager(viewPager);
+
         getDataFromAPI();
     }
 
